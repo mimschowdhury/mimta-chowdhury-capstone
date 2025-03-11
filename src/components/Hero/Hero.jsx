1f756314f45e970fe15b47d6eb2db591ed5380c1
“Mimta <mimta.chowdhury@gmail.com>
2025-03-11 13:13:21 -0400

    add Hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,36 @@
+// import "./Hero.scss";
+
+// function Hero() {
+//     return (
+//         <div className="hero">
+//             <h4 className="hero__title">Our mission:</h4>
+//             <p className="hero__content">
+//             Find Toronto’s Best Cafes — <span className="hero__content-italics">all in one place</span>.
+//             </p>
+//         </div>
+//     );
+// }
+
+// export default Hero;
+
+import "./Hero.scss";
+import heroImage from "../../assets/images/cafe2.jpg"; // Adjust the path to your image
+
+function Hero() {
+  return (
+    <section
+      className="hero"
+      style={{ backgroundImage: `url(${heroImage})`, backgroundSize: 'cover' }}
+    >
+        <div className="hero__text">
+          <h4 className="hero__title">Our mission:</h4>
+          <p className="hero__content">
+            Find Toronto’s Best Cafes — <span className="hero__content-italics">all in one place</span>.
+          </p>
+        </div>
+      <div className="hero__overlay"></div>
+    </section>
+  );
+}
+
+export default Hero;
