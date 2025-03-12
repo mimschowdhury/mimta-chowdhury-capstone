c3c6d3cd3c4483690f2dd80fea62b3dd8de4d1a4
“Mimta <mimta.chowdhury@gmail.com>
2025-03-12 17:40:20 -0400

    add PhotoPage route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './Pages/HomePage'
+import HomePage from './pages/HomePage/HomePage';
+import PhotoPage from './pages/PhotoPage/PhotoPage';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/photos/:id" element={<PhotoPage />} />
       </Routes>
     </BrowserRouter>
   )
