5b683949fb7235fda3e19c7b76fe027067ec99ac
“Mimta <mimta.chowdhury@gmail.com>
2025-03-12 00:44:11 -0400

    add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
