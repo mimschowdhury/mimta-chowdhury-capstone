95b5083ab66ef3eca3bdc9a3deb5e517e31872f9
“Mimta <mimta.chowdhury@gmail.com>
2025-03-12 18:43:40 -0400

    remove unnecessary comments on Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,3 @@
-// import "./Hero.scss";
-
-// function Hero() {
-//     return (
-//         <div className="hero">
-//             <h4 className="hero__title">Our mission:</h4>
-//             <p className="hero__content">
-//             Find Toronto’s Best Cafes — <span className="hero__content-italics">all in one place</span>.
-//             </p>
-//         </div>
-//     );
-// }
-
-// export default Hero;
-
 import "./Hero.scss";
 import heroImage from "../../assets/images/cafe2.jpg"; // Adjust the path to your image
 
