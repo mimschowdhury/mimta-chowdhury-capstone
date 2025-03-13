2b249440f2af79a7db5b26db9322ca61d765f444
“Mimta <mimta.chowdhury@gmail.com>
2025-03-13 06:26:42 -0400

    add AboutPage route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import PhotoPage from './pages/PhotoPage/PhotoPage';
+import AboutPage from './pages/AboutPage/AboutPage';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/photos/:id" element={<PhotoPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   )
