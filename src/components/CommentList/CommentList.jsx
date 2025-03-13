966af1c93d15ffc0b55b1c6893f16a4e6edb381e
“Mimta <mimta.chowdhury@gmail.com>
2025-03-12 23:41:52 -0400

    change logic for comments (number of comments on display)

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -2,10 +2,16 @@ import Comment from "../Comment/Comment";
 import "./CommentList.scss";
 
 function CommentList({ comments }) {
+    const commentCount = comments.length;
+    
     return (
         <div className="comment-list">
-            <h3 className="comment-list__count">{comments.length} Comments</h3> 
-            {comments.map((comment) => (
+            <h3 className="comment-list__count">
+                {commentCount === 0 
+                    ? "No Comments" 
+                    : `${commentCount} ${commentCount === 1 ? 'Comment' : 'Comments'}`}
+            </h3>
+            {commentCount > 0 && comments.map((comment) => (
                 <Comment key={comment.id} comment={comment} />
             ))}
         </div>
