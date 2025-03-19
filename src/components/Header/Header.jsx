cfd4134439282587dccb2718987c4bccaab7050a
“Mimta <mimta.chowdhury@gmail.com>
2025-03-19 06:56:41 -0400

    add scroll mode toggle bar to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,18 @@ import "./Header.scss";
 import Filter from "../Filter/Filter";
 import { Link } from "react-router-dom";
 
-const Header = ({ openDrawer, isDrawerOpen }) => {
+const Header = ({ openDrawer, isDrawerOpen, isGridView, toggleView }) => {
     return (
         <header className="header">
             <Link to="/" className="header__title-link">
                 <h1 className="header__title">6ixCafes</h1>
             </Link>
             <nav className="header__nav">
+                <label className="toggle-switch">
+                    <input type="checkbox" checked={isGridView} onChange={toggleView} />
+                    <span className="slider"></span>
+                    <span className="mode-label">{isGridView ? "Grid Mode" : "Scroll Mode"}</span>
+                </label>
                 <Link to="/about" className="header__nav-link">About</Link>
                 <Link to="/quiz" className="header__nav-link">Discover Cafes</Link>
                 <Link to="/favourites" className="header__nav-link">My Favourites</Link>
