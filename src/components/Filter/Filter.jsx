82dd4020c78a5d23891636736abca7ecf2b6dd06
“Mimta <mimta.chowdhury@gmail.com>
2025-03-20 13:58:25 -0400

    added drop down menu to filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,16 +1,50 @@
+import { useState } from "react";
 import "./Filter.scss";
 import filterIcon from "../../assets/images/Filter.svg";
 
-function Filter({ openDrawer, isDrawerOpen }) { 
+function Filter({ tags, selectedTag, onFilterChange }) {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen((prev) => !prev);
+    };
+
+    const handleSelectChange = (tag) => {
+        onFilterChange(tag === "all" ? null : tag);
+        setIsDropdownOpen(false); // Close dropdown after selection
+    };
+
     return (
         <section className="filter">
-            <button 
-                className={`filter__button ${isDrawerOpen ? "filter__button--active" : ""}`}
-                onClick={openDrawer}
-            >
-                Filters
-                <img src={filterIcon} alt="filter icon" className="filter__button-icon" />
-            </button>
+            <div className="filter__container">
+                <button
+                    className={`filter__button ${isDropdownOpen ? "filter__button--active" : ""}`}
+                    onClick={toggleDropdown}
+                >
+                    Filters
+                    <img src={filterIcon} alt="filter icon" className="filter__button-icon" />
+                </button>
+
+                {isDropdownOpen && (
+                    <div className="filter__dropdown visible">
+                        <button
+                            className={`filter__dropdown-item ${!selectedTag ? "filter__dropdown-item--selected" : ""}`}
+                            onClick={() => handleSelectChange("all")}
+                        >
+                            All Categories
+                        </button>
+                        {tags.map((tag) => (
+                            <button
+                                key={tag.id || tag.name}
+                                className={`filter__dropdown-item ${selectedTag === tag.name ? "filter__dropdown-item--selected" : ""}`}
+                                onClick={() => handleSelectChange(tag.name)}
+                            >
+                                {tag.name}
+                            </button>
+                        ))}
+                    </div>
+                )}
+            </div>
         </section>
     );
 }
