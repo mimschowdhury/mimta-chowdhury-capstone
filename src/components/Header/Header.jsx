ceb0aa08f5635e8bc16a8fe2f065d3367b1792a8
“Mimta <mimta.chowdhury@gmail.com>
2025-03-20 13:58:53 -0400

    passed Filter props to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
+// Header.jsx
 import "./Header.scss";
 import Filter from "../Filter/Filter";
 import { Link } from "react-router-dom";
 
-const Header = ({ openDrawer, isDrawerOpen, isGridView, toggleView }) => {
+const Header = ({ isGridView, toggleView, tags, selectedTag, onFilterChange }) => {
     return (
         <header className="header">
             <Link to="/" className="header__title-link">
@@ -18,7 +19,11 @@ const Header = ({ openDrawer, isDrawerOpen, isGridView, toggleView }) => {
                 <Link to="/quiz" className="header__nav-link">Discover Cafes</Link>
                 <Link to="/favourites" className="header__nav-link">My Favourites</Link>
                 <Link to="/locations" className="header__nav-link">Locations</Link>
-                <Filter openDrawer={openDrawer} isDrawerOpen={isDrawerOpen} />
+                <Filter
+                    tags={tags}
+                    selectedTag={selectedTag}
+                    onFilterChange={onFilterChange}
+                />
             </nav>
         </header>
     );
