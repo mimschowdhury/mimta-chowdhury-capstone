2e8d91069b963597798f4c5931257357e1a61635
“Mimta <mimta.chowdhury@gmail.com>
2025-03-27 12:13:40 -0400

    fix letter casing for photocard for deployment

--- src/components/PhotoCard/PhotoCard.jsx
+++ src/components/PhotoCard/PhotoCard.jsx
@@ -1,33 +1,4 @@
-// import "./Photocard.scss";
-// import { Link } from "react-router-dom";
-
-// function PhotoCard({ url, alt, photographer, tags = [], id }) {
-//   return (
-//     <div className="photocard">
-//       <div className="photocard__content">
-//         <Link to={`/photos/${id}`} className="photocard__link">
-//           <img src={url} alt={alt} />
-//         </Link>
-//         <p className="photocard__photographer">{photographer}</p>
-//       </div>
-//       <ul className="photocard__tags">
-//         {Array.isArray(tags) && tags.length > 0 ? (
-//           tags.map((tag) => (
-//             <li key={tag?.id || tag.name} className="photocard_tag">
-//               {tag?.name || "No Name"}
-//             </li>
-//           ))
-//         ) : (
-//           <li>No tags available</li>
-//         )}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default PhotoCard;
-
-import "./Photocard.scss";
+import "./PhotoCard.scss";
 import { Link } from "react-router-dom";
 
 function PhotoCard({ url, alt, photographer, tags = [], id }) {
